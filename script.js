c498d9a076eb722a094c837d7df0cdd7a2e6ecb9
Kacie O'Neill <hello@kacieo.io>
2024-01-15 16:42:34 -0500

    + functions, answer validation

--- script.js
+++ script.js
@@ -47,3 +47,70 @@ function startQuiz() {
     startTimer();
 }
 
+//displaying questions
+function displayQuestion(){
+    const currentQuestion = quizQuestions[currentQuestionIndex];
+    questionTitle.textContent = currentQuestion.question;
+
+    answerButtons.forEach((button,index) => {
+        button.textContent = currentQuestion.choices[index];
+        button.addEventListener('click', () => checkAnswer(button.textContent));
+    });
+}
+//answer validation and display
+function checkAnswer(userAnswer) {
+    const correctAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
+
+    if (userAnswer === correctAnswer) {
+        displayResult(true);
+        score += 10;
+    } else {
+        displayResult(false);
+        timer -=5;
+    }
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex < quizQuestions.length && timer > 0) {
+        displayQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+function displayResult(isCorrect) {
+    const resultsContainer = document.getElementById('results');
+
+    if(isCorrect) {
+        resultsContainer.textContent = "Correct! Great job!";
+    } else {
+        resultsContainer.textContent = "Sorry, that's not it.";
+
+    }
+}
+//timer
+function startTimer() {
+    const timerInterval = setInterval(() => {
+        timer--;
+
+        if (timer <= 0 || currentQuestionIndex = quizQuestions.length) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+}
+//end of quiz, entering initials
+function endQuiz() {
+    resultsContainer.innerHTML = `
+    <h2>You've completed the quiz!</h2>
+    <p>Your score is ${score}</p>
+    <label for="initials">Enter your initials:</label>
+    <input type="text" id ="initials" />
+    <button onclick="saveScore()">Submit</button>
+    <div id="highScores></div>
+    `;
+    resultsContainer.style.display = 'block';
+}
+
+//saving score
+//display high scores
