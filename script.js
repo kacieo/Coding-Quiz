2fc10cdcc5223e2f2e2b1ba0c25e0ab9375915f8
Kacie O'Neill <hello@kacieo.io>
2024-01-15 20:14:14 -0500

    testing, adding console.log to steps

--- script.js
+++ script.js
@@ -1,43 +1,45 @@
 const startButton = document.getElementById('startButton');
 const quizContainer = document.getElementById('quizContainer');
 const questionTitle = document.getElementById('questionTitle');
-const answerButtons = document.getElementById('answerBtn');
+const answerButtons = document.querySelectorAll('.answerBtn');
 const resultsContainer = document.getElementById('results');
 
 let currentQuestionIndex = 0;
+let score = 0;
 let timer = 75;
 
 //displaying questions
 const quizQuestions = [
     {
-        question: "Commonly used data types *do not* include..."
+        question: "Commonly used data types *do not* include...",
         choices: ["strings","booleans","alerts","numbers"],
         correctAnswer: "alerts"
     },
     {
-        question: "Arrays in JS can be used to store..."
+        question: "Arrays in JS can be used to store...",
         choices: ["numbers and strings","other arrays","booleans","all of the above"],
         correctAnswer: "all of the above"
     },
     {
-        question: "A condition in if/else statement is enclosed with..."
+        question: "A condition in if/else statement is enclosed with...",
         choices: ["quotes", "curly braces", "parentheses", "brackets"],
         correctAnswer: "parentheses"
     },
     {
-        question: "String values must be enclosed with..."
+        question: "String values must be enclosed with...",
         choices: ["commas","curly braces","quotes","parentheses"],
         correctAnswer: "quotes"
     },
     {
-        question: "A very useful tool during development and debugging for printing content to the debugger is..."
-        choices: ["Javascript","terminal/bash","for loops","console log"]
+        question: "A very useful tool during development and debugging for printing content to the debugger is...",
+        choices: ["Javascript","terminal/bash","for loops","console log"],
         correctAnswer: "console log"
     },
 ];
 
 //event listeners to button
 startButton.addEventListener('click', startQuiz);
+
 
 //start quiz
 function startQuiz() {
@@ -52,31 +54,36 @@ function displayQuestion(){
     const currentQuestion = quizQuestions[currentQuestionIndex];
     questionTitle.textContent = currentQuestion.question;
 
-    answerButtons.forEach((button,index) => {
-        button.textContent = currentQuestion.choices[index];
-        button.addEventListener('click', () => checkAnswer(button.textContent));
-    });
+    for (let i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].textContent = currentQuestion.choices[i];
+        answerButtons[i].addEventListener('click', () => checkAnswer(currentQuestion.choices[i]));
+    
+    };
 }
-//answer validation and display
-function checkAnswer(userAnswer) {
-    const correctAnswer = quizQuestions[currentQuestionIndex].correctAnswer;
+    //answer validation and display
+    function checkAnswer(userAnswer) {
+        console.log('Checking answer:', userAnswer);
+        
+        const currentQuestion = quizQuestions[currentQuestionIndex];
+        console.log('Current Q', currentQuestionIndex);
+        let correctAnswer = currentQuestion.correctAnswer;
 
-    if (userAnswer === correctAnswer) {
-        displayResult(true);
-        score += 10;
-    } else {
-        displayResult(false);
-        timer -=5;
+        if (corrrectAnswer.includes(userAnswer)) {
+            displayResult(true);
+            score += 10;
+        } else {
+            displayResult(false);
+            timer -= 10;
+        }
+
+        currentQuestionIndex++;
+
+        if(currentQuestionIndex < quizQuestions.length && timer > 0) {
+            displayQuestion();
+        } else {
+            endQuiz();
+        }
     }
-
-    currentQuestionIndex++;
-
-    if(currentQuestionIndex < quizQuestions.length && timer > 0) {
-        displayQuestion();
-    } else {
-        endQuiz();
-    }
-}
 
 function displayResult(isCorrect) {
     const resultsContainer = document.getElementById('results');
@@ -88,12 +95,15 @@ function displayResult(isCorrect) {
 
     }
 }
-//timer
+//timer 
+//timer counts down in console but does not yet display on page. add container for timer display.
 function startTimer() {
+    console.log('Timer started')
     const timerInterval = setInterval(() => {
+        console.log('Inside setInterval');
         timer--;
-
-        if (timer <= 0 || currentQuestionIndex = quizQuestions.length) {
+        console.log('timer:', timer)
+        if (timer <= 0 || currentQuestionIndex === quizQuestions.length) {
             clearInterval(timerInterval);
             endQuiz();
         }
@@ -107,16 +117,17 @@ function endQuiz() {
     <label for="initials">Enter your initials:</label>
     <input type="text" id ="initials" />
     <button onclick="saveScore()">Submit</button>
-    <div id="highScores></div>
+    <div id="highScores"></div>
     `;
     resultsContainer.style.display = 'block';
 }
 
 //saving score
 function saveScore() {
+    console.log('Saving score');
     const initialsInput = document.getElementById('initials');
     const scoreInput = document.getElementById('score');
-    const initials = initialsInput.ariaValueMax.trim();
+    const initials = initialsInput.value.trim();
     const score = parseInt(scoreInput.value);
 
     if (initials !== '' && !isNaN(score)) {
@@ -137,6 +148,6 @@ function displayHighScores() {
     highScoresContainer.innerHTML = '<h2>High Scores</h2>';
 
     highScores.forEach((score, index) => {
-        highScoresContainer.innerHTML += '<p>${index + 1'.}. ${score.initials} - ${score.score}</p>
+        highScoresContainer.innerHTML += `<p>${index + 1}. ${score.initials} - ${score.score}</p>`
     });
 }
